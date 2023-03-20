6fa92420299a759724a63e3b09df3b1d545cd424
Charllys Fernandes <charllys.daiany@gmail.com>
2023-03-19 21:48:00 -0300

    reform file

--- src/script.js
+++ src/script.js
@@ -1,4 +1,3 @@
-// Captura os elementos HTML que serão utilizados
 const generateButton = document.getElementById("generateButton");
 const copyButton = document.getElementById("copyButton");
 const passwordLengthInput = document.getElementById("passwordLength");
@@ -7,18 +6,15 @@ const mediumRadio = document.getElementsByName("group1")[1];
 const hardRadio = document.getElementsByName("group1")[2];
 const passwordDisplay = document.getElementById("passwordDisplay") // add by human
 
-// Define as variáveis que serão utilizadas
 const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
 const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numberChars = "0123456789";
 const symbolChars = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
 
-// Define a função que irá gerar a senha
 function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
   let password = "";
   let chars = "";
 
-  // Adiciona os caracteres de acordo com as opções selecionadas
   if (includeLowercase) {
     chars += lowercaseChars;
   }
@@ -32,7 +28,6 @@ function generatePassword(length, includeLowercase, includeUppercase, includeNum
     chars += symbolChars;
   }
 
-  // Gera a senha aleatória
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * chars.length);
     password += chars[randomIndex];
@@ -41,7 +36,6 @@ function generatePassword(length, includeLowercase, includeUppercase, includeNum
   return password
 }
 
-// Define a função que será executada ao clicar no botão "Gerar senha"
 generateButton.addEventListener("click", function () {
   const length = parseInt(passwordLengthInput.value);
   const includeLowercase = easyRadio.checked || mediumRadio.checked || hardRadio.checked;
@@ -49,27 +43,21 @@ generateButton.addEventListener("click", function () {
   const includeNumbers = mediumRadio.checked || hardRadio.checked;
   const includeSymbols = hardRadio.checked;
 
-  // Gera a senha e exibe no console
   const password = generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
   console.log(password);
   passwordDisplay.innerText = password
 });
 
-// Define a função que será executada ao clicar no botão "Copiar senha"
 copyButton.addEventListener('click', function () {
-  // Seleciona o elemento que exibe a senha gerada
-  // const passwordDisplay = document.querySelector('#password-display');
-  // Verifica se a senha foi gerada
   const password = passwordDisplay.innerText.length < 1 ? null : passwordDisplay.innerText;
 
-  // Copia a senha para a área de transferência
   if (password) {
     navigator.clipboard.writeText(password).then(() => {
-      alert(`Senha copiada para a área de transferência: ${password}`);
+      alert(`Password copied to clipboard: ${password}`);
     }, () => {
-      alert('Não foi possível copiar a senha para a área de transferência.');
+      alert('Unable to copy password to clipboard.');
     });
   } else {
-    alert('Clique em gerar senha');
+    alert('Click on generate password');
   }
 });
